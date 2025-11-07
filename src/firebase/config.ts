4884f96adb662c2d5ce4ce19fc7d19e2173b4d9b
Queens Mobile <queensmobile11@gmail.com>
2025-11-07 22:41:19 +0000

    I ggot this message from Github
    Google API Key
    Open
    GitHub
    detected a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-4841799393-33b1c",
-  "appId": "1:329746797501:web:3dcf7f023b98cb3395f8d4",
-  "apiKey": "AIzaSyC1V0qDgFvuvjUP2_aPuiQ0JbnxydgQcuQ",
-  "authDomain": "studio-4841799393-33b1c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "329746797501"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
